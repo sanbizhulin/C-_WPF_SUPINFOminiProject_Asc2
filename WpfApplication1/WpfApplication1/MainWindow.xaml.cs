ff05545b1920e922e91a36be067775d1f8998d54
sanbizhulin <167312@supinfo.com>
2017-11-03 17:41:09 +0800

    Upload my project to Github

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           InitializeComponent();
+
+           Page0 page0 = new Page0();
+           frame1.Navigate(page0);
+          
+            page0.ViewTrainer.Click += new RoutedEventHandler(ViewTrainerFucntion);
+           page0.ViewQuestion.Click += new RoutedEventHandler(ViewQuestionFucntion);
+            page0.Validation.Click += new RoutedEventHandler(ValidationFunction);
+
+        }
+        private void ViewTrainerFucntion(object sender, RoutedEventArgs e)
+        {
+            Page1 page1 = new Page1();
+           frame1.Navigate(page1); 
+        }
+            private void ViewQuestionFucntion(object sender, RoutedEventArgs e)
+        {
+            Page2 page2 = new Page2();
+           frame1.Navigate(page2); 
+         }
+        private void ValidationFunction(object sender, RoutedEventArgs e)
+        {
+            Page3 page3 = new Page3();
+           frame1.Navigate(page3); }
+
+
+    }
+    
+    
+    }
+
+
+
---
+++ WpfApplication1/WpfApplication1/Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            //Page2 page2 = new Page2();
+            
+           // page2.List_all_questions.Click += new RoutedEventHandler(List_all_questions_fucntion);
+
+        }
+
+       // private void List_all_questions_fucntion(object sender, RoutedEventArgs e)
+       // {
+       //     Page5 page5=new Page5();
+        //    frame2.Navigate(page5);
+        //}
+    }
+}
